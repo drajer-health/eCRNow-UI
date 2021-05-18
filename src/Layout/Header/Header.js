5c08bfdbebc5a3ee127e90ff9cc5ad1a99edb8ca
Raghunath Sandilya <raghunath.umapathi@xyramsoft.com>
2021-05-18 15:10:11 +0530

    Updated Header text

--- src/Layout/Header/Header.js
+++ src/Layout/Header/Header.js
@@ -17,8 +17,7 @@ class Header extends Component {
       })
     } else if(window.location.pathname === '/healthCareSettings' || window.location.pathname === '/healthCareSettingsList' || window.location.pathname === '/kar' ){
       this.setState({
-        appName:'BSA',
-        appDesc:'(Backend Service App)'
+        appName:'eCRNow/BSA'
       })
     }
   }
